237a85d284fe5d39293c23b548191a08d9a3003c
SilviaWachira <wachirasilvia8@gmail.com>
2022-06-21 13:13:33 -0700

    0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    a = 0
-    for z in range(x):
-        try:
-            print(my_list[z], end="")
-            a += 1
-        except IndexError:
-            break
-    print()
-    return a
+"""Defines a class Square"""
+
+
+class Square:
+    """Just an empty Square class"""
+    pass
